d925f041e595f52097864a593e2923feae2fe9f8
Meinolf Droste <mdwp.post@gmail.com>
2019-04-13 15:08:41 +0200

    scrivito placeholder added

--- src/CountdownWidgetComponent.js
+++ src/CountdownWidgetComponent.js
@@ -7,13 +7,13 @@ import moment from 'moment';
 Scrivito.provideComponent('CountdownWidget', ({ widget }) => {
   const dateInFuture = moment(widget.get('date'), 'YYYY-MM-DD HH:mm:ss');
 
-    /*if (!dateInFuture.length) {
+  if (!dateInFuture.length && Scrivito.isInPlaceEditingActive()) {
     return (
-      <InPlaceEditingPlaceholder center={ true }>
-        Create Accordion Items in widget properties.
-      </InPlaceEditingPlaceholder>
+        <h4 className="text-center">
+          Provide a date in future in the widget properties.
+        </h4>
     );
-  }*/
+  }
 
   return (
     <h3>{widget.get('prefix')}
